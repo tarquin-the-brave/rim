697051dd7b81dd39b9895c1e0628cb3b1f521436
steavot <tomsteavenson@gmail.com>
2018-10-15 22:13:13 +0100

    first attempt, wont compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file = &args[1];
+    let file_type = fs::metadata(&file)?.file_type();
+    if file_type.is_dir() {
+        fs::remove_dir_all(&file);
+    } else {
+        fs::remove_file(&file);
+    }
 }
+

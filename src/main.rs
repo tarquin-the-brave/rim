0956a0cf68bd6335a4ec1298c449f1bf16fd756a
Tom Steavenson <tom.steavenson@metaswitch.com>
2018-10-16 09:09:14 +0100

    fixed one compile error - function that you use ? in has to also return Result

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::fs;
 
 mod dirstack;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let file = &args[1];
     let file_type = fs::metadata(&file)?.file_type();
     if file_type.is_dir() {
-        fs::remove_dir_all(&file);
+        fs::remove_dir_all(&file)
     } else {
-        fs::remove_file(&file);
+        fs::remove_file(&file)
     }
 }
 

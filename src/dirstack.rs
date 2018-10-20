3cc47cb374aeda09bc207985ad74407bf8d13944
steavot <tomsteavenson@gmail.com>
2018-10-20 18:51:06 +0100

    sort out lifetime annotations

--- src/dirstack.rs
+++ src/dirstack.rs
@@ -1,15 +1,15 @@
 // Module to hold the vector of Strings that encodes the directory stack.
 use std::str;
 
-pub struct DirStack {
-    dirstack: Vec<&str>
+pub struct DirStack<'a> {
+    dirstack: Vec<&'a str>
 }
 
-impl DirStack {
+impl<'a> DirStack<'a> {
     // new: pass is a string of space separated directories as in environment variable $DIRSTACK
     // and create a DirStack structure.
-    pub fn new(dirstack_str: String) -> DirStack {
-        DirStack { dirstack: str::split_whitespace(&dirstack_str).collect() }
+    pub fn new(dirstack_str: &'a str) -> DirStack<'a> {
+        DirStack { dirstack: str::split_whitespace(dirstack_str).collect() }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 mod dirstack;
 
 fn main() -> std::io::Result<()> {
+    let dirstack_env = env::var("DIRSTACK").expect("Could not get environment variable DIRSTACK");
     let args: Vec<String> = env::args().collect();
     let file = &args[1];
     let file_type = fs::metadata(&file)?.file_type();

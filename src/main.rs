0b387450d903c17e297e56f77f2656eeada1486a
steavot <tomsteavenson@gmail.com>
2018-10-20 21:34:20 +0100

    multiple files, compile warning about error handling

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,28 @@ use std::fs;
 
 mod dirstack;
 
-fn main() -> std::io::Result<()> {
+fn main() {
+    // Get the directory stack for the session.
     let dirstack_env = env::var("DIRSTACK").expect("Could not get environment variable DIRSTACK");
     let mut dir_stack = dirstack::DirStack::new(dirstack_env.as_str());
+
+    // Take every argument as a list of files to delete.
     let args: Vec<String> = env::args().collect();
+    let mut files = args.clone();
+    files.remove(0);
 
-    let file = &args[1];
-    let file_type = fs::metadata(&file)?.file_type();
+    for file in files.iter() {
+        let file_type = fs::metadata(&file).expect("could not get file type").file_type();
 
-    if file_type.is_dir() {
-        if dir_stack.contains(&file) {
-            dir_stack.remove(&file);
-            env::set_var("DIRSTACK", dir_stack.to_string());
+        if file_type.is_dir() {
+            if dir_stack.contains(&file) {
+                dir_stack.remove(&file);
+                env::set_var("DIRSTACK", dir_stack.to_string());
+            }
+            fs::remove_dir_all(&file);
+        } else {
+            fs::remove_file(&file);
         }
-        fs::remove_dir_all(&file)
-    } else {
-        fs::remove_file(&file)
     }
 }
 
